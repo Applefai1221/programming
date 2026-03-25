3a3721796a0fdf7837d3ab8c9d35204f9bee8289
김준우 <150007870+Applefai1221@users.noreply.github.com>
2026-03-25 14:38:01 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 80 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,5 @@
+phrase = str(input())
+list1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+for i in list1:
+    print(phrase.find(i),end = ' ')
