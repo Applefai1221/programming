376cd7b131217a986a411bf17b5da63bae4884a3
김준우 <150007870+Applefai1221@users.noreply.github.com>
2026-03-25 14:34:20 +0900

    [Silver IV] Title: 수 찾기, Time: 176 ms, Memory: 145304 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,11 @@
+n = int(input())
+list1 = set(map(int,input().split()))
+
+m = int(input())
+list2 = list(map(int,input().split()))
+
+for i in list2:
+    if i in list1:
+        print(1)
+    else:
+        print(0)
